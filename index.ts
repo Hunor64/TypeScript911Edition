6db428812bee2eeaec1da1c6b1920c219bac9ce2
hunor64 <nhunor06@gmail.com>
2024-09-11 13:21:07 +0200

    Refactor code to use TypeScript types consistently

--- index.ts
+++ index.ts
@@ -1,15 +1,27 @@
 const API_URL = "https://pokeapi.co/api/v2/pokemon/pikachu";
 
 type Pokemon = {
-  id: number,
-  base_experience: number,
-  height: number,
-  location_area_encounters: string,
-  is_default: boolean,
+  id: number;
+  base_experience: number;
+  height: number;
+  location_area_encounters: string;
+  is_default: boolean;
+};
+type Ability = {
+  name: string;
+  url: string;
+};
+
+type Abilities = {
+  ability: Ability;
+  is_hidden: boolean;
+  slot: number;
 };
 
 fetch(API_URL)
-.then(response => response.json())
-  .then((data:Pokemon) => {
-    console.log(`${data.id} ${data.base_experience} ${data.height} ${data.location_area_encounters} ${data.is_default}`);
-  })
+  .then((response) => response.json())
+  .then((data: Pokemon) => {
+    console.log(
+      `${data.id} ${data.base_experience} ${data.height} ${data.location_area_encounters} ${data.is_default}`
+    );
+  });
